d5750e558ab5e02ab53f8eaa69d8d3e66d46d22c
sellyjuanalyaaa <alyaasellyjuan@gmail.com>
2024-11-22 08:36:43 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,180 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <div class="navbar">
+        <img src="img/image 9.png" alt="logo psg">
+        <a href="">Menu</a>
+        <a href="">Pemain</a>
+        <a href="">Klub</a>
+        <a href="">Berita</a>
+        <a href="">Toko</a>
+    </div>
+
+    <div>
+        <h1></h1>
+        <h2></h2>
+    </div>
+
+    <div class="carousel">
+        <div class="card-news">
+            <img src="" alt="">
+            <h1></h1>
+        </div>
+        <div class="card-news">
+            <img src="" alt="">
+            <h1></h1>
+        </div>
+        <div class="card-news">
+            <img src="" alt="">
+            <h1></h1>
+        </div>
+        <div class="card-news">
+            <img src="" alt="">
+            <h1></h1>
+        </div>
+        <div class="card-news">
+            <img src="" alt="">
+            <h1></h1>
+        </div>
+    </div>
+   
+    <div class="container">
+        <div class="mvp">
+            <h1>MVP</h1>
+        </div>
+        <div class="pemain-mvp">
+            <div class="pemain">
+                <img src="" alt="">
+            </div>
+            <div class="pemain-text">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="galeri-pemain">
+        <div class="card-pemain">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="card-text">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+        <div class="card-pemain">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="card-text">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+        <div class="card-pemain">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="card-text">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+        <div class="card-pemain">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="card-text">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+        <div class="card-pemain">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="card-text">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+        <div class="card-pemain">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="card-text">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+        <div class="card-pemain">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="card-text">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+        <div class="card-pemain">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="card-text">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+        <div class="card-pemain">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="card-text">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+        <div class="card-pemain">
+            <div class="img">
+                <img src="" alt="">
+            </div>
+            <div class="card-text">
+                <h1></h1>
+                <p></p>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="footer-container">
+            <div class="foot-text">
+                <h1></h1>
+                <p></p>
+                <p></p>
+            </div>
+            <div class="foot-text">
+                <h1></h1>
+                <p></p>
+                <p></p>
+            </div>
+            <div class="foot-text">
+                <h1></h1>
+                <p></p>
+                <p></p>
+            </div>
+            <div class="foot-logo">
+                <img src="" alt="">
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
